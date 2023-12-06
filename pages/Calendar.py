2d67d6cdf9cae6b14eeeb486cf0863e4f98c7bc7
Erick <efm.physics@gmail.com>
2023-12-05 16:27:02 -0800

    adding old changes, calendar seems changed

--- pages/Calendar.py
+++ pages/Calendar.py
@@ -8,48 +8,56 @@ from calendar_view.core.event import Event
 
 from datetime import timedelta
 
-
-
 page_config('Calendar')
 organization, name, authentication_status, username, authenticator = login()
 if authentication_status:
+    week_tab, add_tab, remove_tab = st.tabs(['Weekly Schedule', 'Add', 'Remove'])
+    with week_tab:
+        classes = get_data(organization, 'Classes')
+        events = get_data(organization, 'Events')
+        cycles = get_data(organization, 'Cycles')
+        selected_date = st.date_input('Date', key='display')
+        if selected_date.weekday() == 6:
+            start_date = selected_date
+        else:
+            idx = (selected_date.weekday() + 1) % 7
+            start_date = selected_date - timedelta(idx)
+        end_date = start_date + timedelta(days=6)
+        calendar_config = data.CalendarConfig(dates=f'{start_date} - {end_date}', hours='8:00 - 20:00')
+        calendar_events = []
+        for event in classes.iloc:
+            calendar_events.append(Event(event['class_name'], day=event['date'], start='9:00', end='19:00'))
+        for event in events.iloc:
+            calendar_events.append(Event(event['class_name'], day=event['date'], start='9:00', end='19:00'))
+        for event in cycles.iloc:
+            calendar_events.append(Event(event['class_name'], day=event['date'], start='9:00', end='19:00'))
+        data.validate_config(calendar_config)
+        data.validate_events(calendar_events, calendar_config)
+        calendar = Calendar.build(calendar_config)
+        calendar.add_events(calendar_events)
+        calendar.save("cal.png")
+        st.image('cal.png')
+    with add_tab:
+        # form = st.form('add_form')
+        l_col, r_col = st.columns([1,1])
+        event_name = l_col.text_input('Name')
+        is_class = st.selectbox('Type', ['Class', 'Event', 'Cycle'])
+        recurring = st.checkbox('Recurring')
+        if recurring:
+            frequency = l_col.selectbox('Frequency', ['Daily', 'Weekly', 'Monthly', 'Annually'])
+            start_date = r_col.date_input('Start Date')
+            end_date = r_col.date_input('End Date')
+        else:
+            event_date = r_col.date_input('Date')
+        if st.button('Add'):
+            if recurring:
+                st.success(f'{event_name}{recurring}{frequency}{start_date}{end_date}')
+            else:
+                st.success(f'{event_name}{recurring}{event_date}')
 
-    classes = get_data(organization, 'Classes')
-    events = get_data(organization, 'Events')
-    cycles = get_data(organization, 'Cycles')
-    selected_date = st.date_input('Date')
-    if selected_date.weekday() == 6:
-        start_date = selected_date
-    else:
-        idx = (selected_date.weekday() + 1) % 7
-        start_date = selected_date - timedelta(idx)
-    end_date = start_date + timedelta(days=6)
-    calendar_config = data.CalendarConfig(dates=f'{start_date} - {end_date}', hours='8:00 - 20:00')
-    calendar_events = []
 
-    for event in classes.iloc:
-        calendar_events.append(Event(event['class_name'], day=event['date'], start='9:00', end='19:00'))
-    for event in events.iloc:
-        calendar_events.append(Event(event['class_name'], day=event['date'], start='9:00', end='19:00'))
-    for event in cycles.iloc:
-        calendar_events.append(Event(event['class_name'], day=event['date'], start='9:00', end='19:00'))
-
-    #     [
-    #     Event('Planning', day='2019-09-23', start='11:00', end='13:00'),
-    #     Event('Class', day='2019-09-23', start='10:00', end='14:00'),
-    #     Event('Class', day='2019-09-23', start='5:00', end='23:00'),
-    #     Event('Class', day='2019-09-23', start='5:00', end='23:00'),
-    #     Event('Demo', day='2019-09-27', start='15:00', end='16:00'),
-    #     Event('Retrospective', day='2019-09-27', start='17:00', end='18:00'),
-    # ]
-
-    data.validate_config(calendar_config)
-    data.validate_events(calendar_events, calendar_config)
-
-    calendar = Calendar.build(calendar_config)
-    calendar.add_events(calendar_events)
-    calendar.save("cal.png")
-    st.image('cal.png')
+    with remove_tab:
+        pass
 elif authentication_status is None:
     pass
 elif not authentication_status:
